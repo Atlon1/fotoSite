d56d5bd782dfba875abecf2b8cefab241d205a7d
Atlon1 <maciej.szajstek@gmail.com>
2023-07-14 21:10:15 +0200

    first commit

---
+++ src/request.tsx
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from 'axios';
+
+
+export const request: AxiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL as string,
+    headers: {
+        Authorization: 'bearer ' + (process.env.REACT_APP_API_KEY as string),
+    },
+});
