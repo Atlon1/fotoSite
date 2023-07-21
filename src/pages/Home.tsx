028065a5bd3bd3eddc9b42048a0ed217ccac59ad
Atlon1 <maciej.szajstek@gmail.com>
2023-07-21 23:10:20 +0200

    fech is prepare

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
+// @ts-ignore
+import useFetch from '../hooks/useFetch'
+
 const Home = () => {
+    const  { data } = useFetch('/products?populate=*')
+
+    console.log(data)
     return (
         <div>
             Home
--- src/request.tsx
+++ src/request.tsx
@@ -4,6 +4,6 @@ import axios, { AxiosInstance } from 'axios';
 export const request: AxiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL as string,
     headers: {
-        Authorization: 'bearer ' + (process.env.REACT_APP_API_KEY as string),
+        Authorization: 'bearer ' + (process.env.REACT_APP_API_TOKEN as string),
     },
 });
