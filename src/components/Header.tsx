da6c755fbdde2c246a8d53e63eac539cf4c5161a
Atlon1 <maciej.szajstek@gmail.com>
2023-07-27 15:41:50 +0200

    task rdy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
 
+import Logo from '../img/logo.png';
+
+import {SlBag} from 'react-icons/sl'
+import {FiMenu} from 'react-icons/fi'
+
+import {Link} from 'react-router-dom'
+
+import SearchForm from "./SearchForm";
+import CategoryNavMobile from "./CategoryNavMobile";
+import Cart from "./Cart";
+
+
+
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header>
+            <div className='container mx-auto'>
+                <div>
+                    <div>
+                        <FiMenu/>
+                    </div>
+                    <div>
+                        <CategoryNavMobile/>
+                    </div>
+                    <Link to={'/'}>
+                        <img src={Logo} alt='Logo'/>
+                    </Link>
+                    <div className='hidden w-full xl:flex xl:max-w-[734px]'>
+                        <SearchForm/>
+                    </div>
+                    <div>
+                        <div>Need help? 123 456 789</div>
+                        <div className='relative cursor-pointer'>
+                            <SlBag className='text-2xl'/>
+                            <div>2</div>
+                        </div>
+                        <div className='bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 md:mx-w-[500px] transition-all duration-300'>
+                            <Cart/>
+                        </div>
+                    </div>
+                </div>
+                <div className='lg:hidden'>
+                    <SearchForm/>
+                </div>
+            </div>
+        </header>
     );
 };
 
