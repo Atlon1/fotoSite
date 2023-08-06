f41513baa5444551a5ce25c4fe61e59e975fc164
Atlon1 <maciej.szajstek@gmail.com>
2023-08-06 21:23:01 +0200

    section header countinue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,20 +13,21 @@ import Cart from "./Cart";
 import {CartContext} from "../context/CartContext";
 
 
-
 const Header = () => {
-   const {isOpen, setIsOpen} = useContext<any>(CartContext)
+    const {isOpen, setIsOpen} = useContext<any>(CartContext)
     const [catNavMobile, setCatNavMobile] = useState(false)
     return (
         <header className='bg-primary py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px]'>
             <div className='container mx-auto'>
                 <div className='flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0'>
                     <div
-                        onClick={()=> setCatNavMobile(true)}
+                        onClick={() => setCatNavMobile(true)}
                         className='text-3xl xl:hi cursor-pointer'>
                         <FiMenu/>
                     </div>
-                    <div className={`${catNavMobile ? "left-0" : "-left-full"} fixed top-0 bottom-0 z-30 w-full h-screen transition-all duration-200`}>
+                    <div
+
+                        className={`${catNavMobile ? "left-0" : "-left-full"} fixed top-0 bottom-0 z-30 w-full h-screen transition-all duration-200`}>
                         <CategoryNavMobile setCatNavMobile={setCatNavMobile}/>
                     </div>
                     <Link to={'/'}>
@@ -35,14 +36,18 @@ const Header = () => {
                     <div className='hidden w-full xl:flex xl:max-w-[734px]'>
                         <SearchForm/>
                     </div>
-                    <div>
-                        <div>Need help? 123 456 789</div>
-                        <div onClick={()=>(setIsOpen(!isOpen))}
+                    <div className='flex items-center gap-x-[10px]'>
+                        <div className='hidden xl:flex uppercase'>Need help? 123 456 789</div>
+                        <div onClick={() => (setIsOpen(!isOpen))}
                              className='relative cursor-pointer'>
                             <SlBag className='text-2xl'/>
-                            <div>2</div>
+                            <div className='bg-accent text-primary absolute w-[18px] h-[18px] rounded-full top-3 -right-1 text-[13px]
+                            flex justify-center items-center font-bold tracking-[-0.1em]'>
+                                2
+                            </div>
                         </div>
-                        <div className={`${isOpen ? "right-0" : "-right-full"} bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 md:max-w-[500px] transition-all duration-300`}>
+                        <div
+                            className={`${isOpen ? "right-0" : "-right-full"} bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 md:max-w-[500px] transition-all duration-300`}>
                             <Cart/>
                         </div>
                     </div>
