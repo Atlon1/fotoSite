0bc0d9f1f9a3ecbd69f751b8c7bf5ead9c4cb503
Atlon1 <maciej.szajstek@gmail.com>
2023-08-13 21:57:18 +0200

    serach logic ready

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,18 +1,41 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {FiSearch} from 'react-icons/fi'
 import {useNavigate} from "react-router-dom"
 
 const SearchForm = () => {
     const navigate = useNavigate()
     const [searchItem, setSearchItem] =useState('')
+    const [isAnimating, setIsAnimating] = useState(false)
+
+    useEffect(()=> {
+        const timeout = setTimeout(()=>{
+            setIsAnimating(false)
+        },1000)
+        return ()=> clearTimeout(timeout)
+    })
 
     const handleSerachInput = (e : any) => {
+        setSearchItem(e.target.value)
+    }
 
+    const handleSubmit = (e: any)=>{
+        e.preventDefault()
+        if (searchItem.length > 0){
+            navigate(`/search?query=${searchItem}`)
+            // @ts-ignore
+            document.querySelector('input').value = ''
+            setSearchItem('')
+        } else {
+            setIsAnimating(true)
+        }
     }
 
     return (
-        <form className='w-full relative'>
+        <form
+            onSubmit={handleSubmit}
+            className={`${isAnimating ? 'animate-shake' : 'animate-none'} w-full relative`}>
             <input
+                onChange={handleSerachInput}
                 className='input'
                 type='text'
                 placeholder='Search for a product..'/>
